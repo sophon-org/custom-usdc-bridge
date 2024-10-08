a3355b4a2abb0ea8c1db6b7748b8430916d1cd46
Federico Martín Alconada Verzini <fedealconada@gmail.com>
2024-10-08 13:01:53 +0100

    chore: update DeployL1SharedBridge script and upgrade

--- script/DeployL1SharedBridge.s.sol
+++ script/DeployL1SharedBridge.s.sol
@@ -4,7 +4,10 @@ pragma solidity ^0.8.13;
 import {Script, console} from "forge-std/Script.sol";
 import {L1SharedBridge} from "../src/L1SharedBridge.sol";
 import {IBridgehub} from "@era-contracts/l1-contracts/contracts/bridgehub/IBridgehub.sol";
-import {TransparentUpgradeableProxy} from "@openzeppelin/contracts/proxy/transparent/TransparentUpgradeableProxy.sol";
+import {
+    ITransparentUpgradeableProxy,
+    TransparentUpgradeableProxy
+} from "@openzeppelin/contracts/proxy/transparent/TransparentUpgradeableProxy.sol";
 import {DeploymentUtils} from "../utils/DeploymentUtils.sol";
 
 contract DeployL1SharedBridge is Script, DeploymentUtils {
@@ -25,20 +28,39 @@ contract DeployL1SharedBridge is Script, DeploymentUtils {
         return _run(networkName, configName);
     }
 
-    function _run(string memory networkName, string memory configName) internal returns (address, address) {
+    function _run(string memory networkName, string memory configName)
+        internal
+        returns (address sharedBridgeProxy, address sharedBridgeImpl)
+    {
         // TODO: set proper addresses, maybe read from env
         address deployerAddress = msg.sender;
         address proxyAdmin = vm.envAddress("PROXY_ADMIN");
 
         vm.startBroadcast();
 
-        L1SharedBridge sharedBridgeImpl =
-            new L1SharedBridge(getDeployedContract("USDC"), IBridgehub(getDeployedContract("Bridgehub")));
+        sharedBridgeProxy = getDeployedContract("L1SharedBridge");
 
-        TransparentUpgradeableProxy sharedBridgeProxy = new TransparentUpgradeableProxy(
-            address(sharedBridgeImpl),
-            proxyAdmin,
-            abi.encodeWithSelector(L1SharedBridge.initialize.selector, deployerAddress)
+        // deploy implementation
+        sharedBridgeImpl =
+            address(new L1SharedBridge(getDeployedContract("USDC"), IBridgehub(getDeployedContract("Bridgehub"))));
+
+        // if proxy exists, upgrade proxy with new implementation
+        if (sharedBridgeProxy != address(0)) {
+            console.log("Upgrading L1SharedBridge");
+            if (msg.sender != proxyAdmin) revert("Only proxy admin can upgrade the implementation");
+            ITransparentUpgradeableProxy(payable(sharedBridgeProxy)).upgradeTo(sharedBridgeImpl);
+            console.log("L1SharedBridge implementation upgraded @", sharedBridgeImpl);
+            saveDeployedContract("L1SharedBridge-impl", sharedBridgeImpl);
+            return (sharedBridgeProxy, sharedBridgeImpl);
+        }
+
+        // deploy proxy
+        sharedBridgeProxy = address(
+            new TransparentUpgradeableProxy(
+                sharedBridgeImpl,
+                proxyAdmin,
+                abi.encodeWithSelector(L1SharedBridge.initialize.selector, deployerAddress)
+            )
         );
 
         console.log("L1SharedBridge implementation deployed @", address(sharedBridgeImpl));
